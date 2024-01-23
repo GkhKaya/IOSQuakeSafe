0ecf73a5ef4ccf431eb75c0abadde90230684544
GkhKaya <gkhkaya00@gmail.com>
2024-01-23 22:54:43 +0300

    First Commit

---
+++ IOSQuakeSafe/IOSQuakeSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSQuakeSafeApp.swift
+//  IOSQuakeSafe
+//
+//  Created by Gokhan Kaya on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSQuakeSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
